e8d1ea313982088bc708ee72b748838c2fa8cd5a
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2020-09-28 22:53:37 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -46,7 +46,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://github.com/DARKCYBERGANG/FUGAKU_THE_UBOT)**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[FUGAKU](https://github.com/DARKCYBERGANG/FUGAKU_THE_UBOT)**\n\n"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
