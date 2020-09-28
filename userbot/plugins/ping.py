1af8a01bb3f78739f537f14f922f43677eb1871d
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2020-09-28 08:41:32 +0000

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -45,4 +45,4 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
-    await event.edit(f"🏓Ping speed: {ms}\n🤖TeleBot Uptime: {uptime}")
+    await event.edit(f"🏓Ping speed: {ms}\n🤖Fugaku Uptime: {uptime}")
