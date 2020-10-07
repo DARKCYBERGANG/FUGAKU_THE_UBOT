e59853b6243300720be9dbb2d247b80996d88d1d
LEGENDARY KEYS <70282108+Keys-007@users.noreply.github.com>
2020-10-07 22:46:32 +0530

    Update Apapa.py

--- userbot/plugins/Apapa.py
+++ userbot/plugins/Apapa.py
@@ -11,7 +11,7 @@ async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("Sunn be lawde")
         await asyncio.sleep(0.7)
-        await event.edit("Baap Baap hota h")
+        await event.edit("Baap Baap hota hai")
         await asyncio.sleep(1)
         await event.edit("Baap ke samne Bakchodi nhi")
         await asyncio.sleep(0.8)
