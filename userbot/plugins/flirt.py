f7092a8dd4a2e724af377ad6871755e49c13ce81
DARKCYBERGANG <71146813+DARKCYBERGANG@users.noreply.github.com>
2020-10-18 19:32:48 +0530

    Add files via upload

--- userbot/plugins/flirt.py
+++ userbot/plugins/flirt.py
@@ -13,9 +13,9 @@ from userbot.utils import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("speaking truth about u.......")
+    await event.edit("Speaking The Truth About You 😃😃🙂🙂.......")
     await asyncio.sleep(2)
-    x=(random.randrange(1,25))
+    x=(random.randrange(1,72))
     if x==1:
         await event.edit("They say Disneyland is the happiest place on earth. Well apparently, no one has ever been standing next to you! **")
     if x==2:
@@ -25,7 +25,7 @@ async def _(event):
     if x==4:
         await event.edit("Was your dad a boxer? Because damn, you’re a knockout!")
     if x==5:
-       await event.edit("I was wondering if you had an extra heart. Mine was just stolen. ") 
+        await event.edit("I was wondering if you had an extra heart. Mine was just stolen By You . ") 
     if x==6:
         await event.edit("Did the sun come out or did you just smile at me? ")
     if x==7:
@@ -51,7 +51,7 @@ async def _(event):
     if x==17:
         await event.edit("Sorry, but you owe me a drink. [Why?] Because when I looked at you, I dropped mine.")
     if x==18:
-       await event.edit("You must be a broom, ‘cause you just swept me off my feet. ")
+        await event.edit("You must be a broom, ‘cause you just swept me off my feet. ")
     if x==19:
         await event.edit("Have you been to the doctor’s lately? Cause I think you’re lacking some vitamin me.")
     if x==20:
@@ -65,6 +65,78 @@ async def _(event):
     if x==24:
         await event.edit("Can I borrow a kiss? I swear I’ll give it back. ")
     if x==25:
-        await event.edit("Written and Created By: @The_Avengers_leader join @pyfilestonystark ! thank you🙏🏻❤")
-
- 
+  	  await event.edit("Your lips look lonely would they like to meet mine?")
+    if s==26:
+        await event.edit("There isn’t a word in the dictionary to describe how beautiful you are")
+    if s==27:
+        await event.edit("I have had a really bad day and it always makes me feel better to see a pretty girl smile. So, would you smile for me?")
+    if s==28:
+        await event.edit("I lost my teddy bear can i sleep with you tonight?")
+    if s==29:
+        await event.edit("I’m no organ donor but I’d be happy to give you my heart.")
+    if s==30:
+        await event.edit("If I had to rate you out of 10 I’d rate you a 9… because I am the one that you are missing")
+    if s==31:
+        await event.edit("Can I follow you? Cause my mom told me to follow my dreams")
+    if s==32:
+        await event.edit("Your hand looks heavy can i hold it for you?")
+    if s==33:
+        await event.edit("You may fall from the sky, you may fall from a tree, but the best way to fall… is in love with me.")
+    if s==34:
+        await event.edit("Are you the sun? Because you’re so beautiful it’s blinding me")
+    if s==35:
+        await event.edit("I should call you Google, because you have everything I’m looking for.")
+    if s==36:
+        await event.edit("Can you kiss me on the cheek so I can at least say a cute girl kissed me tonight?")
+    if x==37:
+        await event.edit(" You may fall from the sky,\n you may fall from a tree, \n but the best way to fall�\n is in love with me. ")
+    if x==38:
+        await event.edit("Do you have a name or can I call you mine?")
+    if x==39:
+        await event.edit("You know you�re in love when you can�t fall asleep because reality is finally better than your dreams.")
+    if x==40:
+        await event.edit("Guess what I�m wearing? The smile you gave me!")
+    if x==51:
+        await event.edit("My friends bet I can�t talk to the prettiest girl. Wanna use their money to buy drinks?")
+    if x==52:
+        await event.edit("You look familiar, didn�t we take a class together? I could�ve sworn we had chemistry.")
+    if x==53:
+        await event.edit("When I look into your eyes, it is like a gateway into the world of which I want to be a part.")
+    if x==54:
+        await event.edit("Keep an eye out for elves with ropes and a blindfold! Why? Cause I asked Santa for you this Christmas")
+    if x==55:
+        await event.edit("Are you the ocean? Cuz baby I want to swim in you all day")
+    if x==56:
+        await event.edit("I just got dumped, and I think that you could make me feel better.")
+    if x==57:
+        await event.edit("Are you netflix? Because i could watch you for hours.")
+    if x==58:
+        await event.edit("When I�m older, I�ll look back at all of my crowning memories, and I�ll think of the day my children were born, the day I got married, and the day that I met you.")
+    if x==59:
+        await event.edit("Wait, something is really wrong with my cell phone. I�m not sure what happened but your number is not in it. May I have it again?")
+    if x==60:
+        await event.edit("I think your hand looks heavy. Would you like me to hold it for you?")
+    if x==61:
+        await event.edit("Damn, you�re so gorgeous you made me forget what my pick up line was.")
+    if x==62:
+        await event.edit("Are you lost? Because heaven�s a long way of here.")
+    if x==63:
+        await event.edit("Boy: I bet your feet are feeling tired now.\n Girl: Why?\n Boy: Because you�ve been running through my mind day and night.")
+    if x==64:
+        await event.edit("Can I take a picture of you so I could show Santa what I want for Christmas?")
+    if x==65:
+        await event.edit("Can you recommend a bank where I can make a deposit? Because I�m planning to save all my love for you.")
+    if x==66:
+        await event.edit("If a thousand painters worked for a thousand years, they could not create a work of art as beautiful as you.")
+    if x==67:
+        await event.edit("Do you believe in love at first sight or should I walk by again?")
+    if x==68:
+    	await event.edit("If I could rearrange the alphabet, I would put U and I together.")
+    if x==69:
+    	await event.edit("If I were a stop light, I would always turn red each time you pass by. In that way, I could stare at you longer.")
+    if x==70:
+        await event.edit("God has provided us with two ears, two eyes and two hands. But He only gave us one heart. He told me to find you and tell you that you�re the second one.")
+    if x==71:
+        await event.edit("Please don�t go now. Else, I would have to go to the police station and report you to the cops. You just stole my heart.")
+    if x==72:
+        await event.edit("You know what I�m going to do now? I�ll put a tear drop in the sea. When you find it, it�s the time I�ll stop loving you. Deal?")
